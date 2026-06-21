400702c8ef2a854e4dbde7e9e7b99f43043b82ac
Bharatt16 <194278228+Bharatt16@users.noreply.github.com>
2026-06-21 23:08:54 +0530

    Initial Project setup

---
+++ index.js
@@ -0,0 +1,19 @@
+import http from 'node:http';
+import { Server } from 'socket.io';
+import path from 'node:path';
+import express from 'express';
+
+async function main(){
+    const app = express()
+    app.use(express.static(path.resolve('./public')))
+
+    const server = http.createServer(app)
+    const io = new Server(server)
+
+    io.attach(server)
+
+    server.listen(3000,()=>{
+        console.log('Server is running on port 3000')
+})
+}
+main()
